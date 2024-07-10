766a77d24c61b03a9d9711d32a1230f730d219c1
HeritageHigherInstitute <156737269+HeritageHigherInstitute@users.noreply.github.com>
2024-07-10 03:07:28 +0000

    made some change

--- database.py
+++ database.py
@@ -10,5 +10,15 @@ def load_jobs_from_db():
     result = conn.execute(text("select * from jobs"))
     jobs = []
     for row in result.all():
-      jobs.append(dict(row))
+      jobs.append(row)
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {"val": id})
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]
